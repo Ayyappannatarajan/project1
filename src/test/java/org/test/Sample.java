401576aed75b91106f139606d090f7684946b627
Ayyappannatarajan <ayyappanmech012@gmail.com>
2022-03-18 11:24:50 +0530

    new code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -1,7 +1,13 @@
 package org.test;
 
+import org.testng.annotations.Test;
+
 public class Sample {
-	public static void main(String[] args) {
+	@Test
+	private void test() {
+
+
+	
 		System.out.println("test1");
 		System.out.println("test2");
 		System.out.println("test3");
